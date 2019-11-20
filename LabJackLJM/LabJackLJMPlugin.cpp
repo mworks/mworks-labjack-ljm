152634157de926b6d0856410665cd5ecd320d5e0
Christopher Stawarz <cstawarz@mit.edu>
2019-11-20 14:38:51 -0500

    Basic build setup

---
+++ LabJackLJM/LabJackLJMPlugin.cpp
@@ -0,0 +1,9 @@
+//
+//  LabJackLJMPlugin.cpp
+//  LabJackLJM
+//
+//  Created by Christopher Stawarz on 11/20/19.
+//  Copyright © 2019 The MWorks Project. All rights reserved.
+//
+
+#include <stdio.h>
